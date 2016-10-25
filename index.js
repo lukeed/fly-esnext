26de9adf56adb84a256403a01c523a61350258ec
Luke Edwards <luke.edwards05@gmail.com>
2016-10-25 10:56:54 +0200

    new plugin logic; rewrite async-->gen with regex

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-require('babel/register');
+'use strict';
+
+const Script = require('vm').Script;
+const req = require('require-like');
+
+const fn = 'function*';
+const box = {};
+
+module.exports = function (file, data) {
+	// setup mock env
+	Object.assign(box, global);
+	box.module = {exports: exports};
+	box.exports = exports;
+	box.require = req(file);
+
+	const scr = new Script(
+		data.replace(new RegExp('await', 'gi'), 'yield')
+		.replace(/export /gi, 'exports.')
+		.replace(/default async function/gi, `default = ${fn}`)
+		.replace(/async function(\s)?.+?(?=\()/gi, str => str.trim().split(' ').pop().concat(` = ${fn} `))
+	);
+	// `eval()` new content
+	scr.runInNewContext(box);
+	// return new `tasks` object
+	return box.module.exports;
+};
