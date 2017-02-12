89280775dd036b1bbf7c2a93909bcbfc2b5256b4
Luke Edwards <luke.edwards05@gmail.com>
2017-02-11 19:45:45 -0800

    add tests for multiple params; update exsiting

--- test/fixtures/flyfile.js
+++ test/fixtures/flyfile.js
@@ -5,11 +5,19 @@ export default async function () {
   await this.source('src/*.js').target('dist');
 };
 
-export async function first () {
+export async function foo () {
   await this.clear('dist');
-  return await this.start('second', {val: aaa});
+  return await this.start('bar', {val: aaa});
 }
 
-export async function second(o) {
+export async function bar(o) {
   return `${foo()}: ${o ? o.val : aaa}`;
+}
+
+export async function baz(one, two) {
+  return {one, two};
+}
+
+export async function bat (one, two) {
+  return {one, two};
 }
--- test/index.js
+++ test/index.js
@@ -9,25 +9,37 @@ const fn = require('../');
 const file = join(__dirname, 'fixtures', 'flyfile.js');
 const data = read(file, 'utf8');
 
-function isYieldable(fn) {
-	const str = fn.toString();
-	return /function*/.test(str) && /yield/.test(str);
-}
+const hasYield = f => /yield/i.test(f.toString());
+const isGenerator = f => f.constructor.name === 'GeneratorFunction';
 
 test('fly-esnext', co(function * (t) {
+	t.plan(25);
+
 	t.equal(typeof fn, 'function', 'export a function');
 
 	const out = fn(file, data);
+
 	t.equal(typeof out, 'object', 'return an object');
-	t.equal(Object.keys(out).length, 3, 'preserve all task names');
-	t.equal(typeof out.default, 'function', 'preserve tasks as functions');
+	t.equal(Object.keys(out).length, 5, 'exports all tasks');
 
-	t.true(isYieldable(out.default), 'rewrite `default` task');
-	t.true(isYieldable(out.first), 'rewrite `first` task');
-	t.true(/function\* \(o\)/.test(out.second.toString()), 'retain `second` task parameter');
+	let func;
+	['default', 'foo', 'bar', 'baz', 'bat'].forEach(k => {
+		func = out[k];
+		t.true(func !== undefined, `exports.${k} is defined`);
+		t.equal(typeof func, 'function', `exports.${k} is a function`);
+		t.true(isGenerator(func), `rewrite exports.${k} as a generator function`);
+	});
 
-	const val = yield co(out.second)();
-	t.equal(val, 'hello: 42', 'handle `require()` & embedded values');
+	// specifics
+	t.true(hasYield(out.foo), '`foo` task has `yield`');
+	t.true(hasYield(out.default), '`default` task has `yield`');
+	t.true(/function\* \(o\)/.test(out.bar.toString()), 'keeps one parameter');
+	t.true(/function\* \(one, two\)/.test(out.baz.toString()), 'keeps two parameters (xo)');
+	t.true(/function\* \(one, two\)/.test(out.bat.toString()), 'keeps two parameters (standard)');
 
-	t.end();
+	const val1 = yield co(out.bar)();
+	t.equal(val1, 'hello: 42', 'handle `require()` & embedded values');
+
+	const val2 = yield co(out.baz)('foo', 'bar');
+	t.deepEqual(val2, {one: 'foo', two: 'bar'}, 'accepts & handles multiple parameters');
 }));
